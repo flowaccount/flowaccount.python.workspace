cf1b4226b70f2779e04b66287c269081eef41de6
Pipat Methavanitpong <pipat_m@flowaccount.com>
2022-03-04 22:21:41 +0700

    Clean open platform exports

--- src/py/etl/open_platform_status/handlers/clean_open_platform.py
+++ src/py/etl/open_platform_status/handlers/clean_open_platform.py
@@ -1,4 +1,5 @@
 import json
+import re
 import urllib.parse
 from typing import Dict, List, Tuple
 
@@ -9,6 +10,14 @@ import pandas as pd
 s3 = boto3.client("s3")
 clean_bucket = "pipat-clean-bucket"
 clean_catalog = "clean"
+
+
+def format_snake_case(camel_case: str) -> str:
+    pattern = re.compile(r"[A-Z]")
+    lower_words = pattern.split(camel_case)
+    big_chars = pattern.findall(camel_case)
+    words = [t[0] + t[1] for t in zip([""] + big_chars, lower_words)]
+    return ("_").join(words).lower()
 
 
 def get_manifest_from_event(
@@ -105,6 +114,80 @@ def clean_manifest_files(
     return df
 
 
+def clean_files(
+    files_df: pd.DataFrame, export_id: str, bucket: str, year: int, month: int
+) -> pd.DataFrame:
+    def format_column_name(col: str) -> str:
+        name = col.rsplit(".", maxsplit=1)[0]
+        return format_snake_case(name)
+
+    df_list = [
+        wr.s3.select_query(
+            sql="SELECT * FROM s3object[*]",
+            path=f"s3://{bucket}/{key}",
+            input_serialization="JSON",
+            input_serialization_params={"Type": "Document"},
+            compression="gzip",
+        )
+        for key in files_df["data_file_s3_key"]
+    ]
+
+    df = pd.concat(df_list)
+    df = pd.json_normalize(df["Item"])
+
+    # Rename columns e.g. companyId.N --> company_id
+    df = df.rename(columns=format_column_name)
+
+    # Fill NaN with -1 for integer columns
+    for col in [
+        "company_id",
+        "payment_channel_id",
+        "expires_in",
+        "refresh_expires_in",
+        "user_id",
+    ]:
+        df[col] = df[col].fillna(-1)
+
+    df = df.astype(
+        {
+            "company_id": "int64",
+            "shop_id": "string",
+            "platform_info": "string",
+            "is_delete": "bool",
+            # 'expired_at': 'int64',
+            "payment_channel_id": "int64",
+            # 'created_at': 'int64',
+            "expires_in": "int64",
+            "is_vat": "bool",
+            "payload": "string",
+            "guid": "string",
+            "refresh_expires_in": "int64",
+            "user_id": "int64",
+            # 'updated_at': 'int64',
+            "platform_name": "string",
+            "refresh_token": "string",
+            "remarks": "string",
+            "access_token": "string",
+            "email": "string",
+        }
+    )
+
+    # Convert datetime string to datetime64
+    df["expired_at"] = pd.to_datetime(df["expired_at"], unit="s")
+    df["created_at"] = pd.to_datetime(df["created_at"], unit="s")
+    df["updated_at"] = pd.to_datetime(df["updated_at"], unit="s")
+
+    # NOTE: Pandas cannot write timedelta to parquet files. It needs 'fastparquet' engine.
+    # records['expires_in'] = pd.to_timedelta(records['expires_in'], unit='s')
+    # records['refresh_expires_in'] = pd.to_timedelta(records['refresh_expires_in'], unit='s')
+
+    df["export_id"] = export_id
+    df["year"] = year
+    df["month"] = month
+
+    return df
+
+
 def handle(event, context):
     # Extract S3 file URI
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
@@ -129,6 +212,9 @@ def handle(event, context):
     month = summary_df["month"][0]
     files_df = clean_manifest_files(manifest_files, export_id, year, month)
 
+    # Clean exported open platform table
+    table_df = clean_files(files_df, export_id, bucket, year, month)
+
     # Create Glue database catalog if not exists
     databases = wr.catalog.databases()
     if clean_catalog not in databases.values:
@@ -152,6 +238,17 @@ def handle(event, context):
         partition_cols=["year", "month"],
         database=clean_catalog,
         table="dynamodb_export_manifest_files",
+    )
+
+    # Write table records
+    table = summary_df["table_arn"][0].rsplit("/")[1]
+    wr.s3.to_parquet(
+        df=table_df,
+        path=f"s3://{clean_bucket}/dynamodb/tables/{table}",
+        dataset=True,
+        partition_cols=["year", "month"],
+        database=clean_catalog,
+        table=f"dynamodb_{table}",
     )
 
     return {"statusCode": 200}
