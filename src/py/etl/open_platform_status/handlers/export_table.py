a4ee6e508c53a922cdacc71d3b21a6b1cf85ced4
Pipat Methavanitpong <pipat_m@flowaccount.com>
2022-03-09 15:34:01 +0700

    Create export dynamodb table lambda

---
+++ src/py/etl/open_platform_status/handlers/export_table.py
@@ -0,0 +1,37 @@
+import boto3
+from datetime import datetime
+import uuid
+
+client = boto3.client("dynamodb")
+dynamodb_arn = "arn:aws:dynamodb:ap-southeast-1:697698820969"
+raw_bucket = "pipat-raw-bucket"
+s3_prefix = "dynamodb/tables"
+
+def handle(event, context):
+    """
+    Start DynamoDB Export to S3 job
+    """
+
+    table_name = event["table"]
+    table_arn = f"{dynamodb_arn}:{table_name}"
+    if "export_time" in event:
+        export_time = datetime.fromisoformat(event["export_time"])
+    else:
+        export_time = datetime.now()
+    client_token = str(uuid.uuid4())
+
+    result = client.export_table_to_point_in_time(
+        TableArn=table_arn,
+        ExportTime=export_time,
+        ClientToken=client_token,
+        S3Bucket=raw_bucket,
+        S3Prefix=f"{s3_prefix}/{table_name}",
+        ExportFormat="DYNAMODB_JSON"
+    )
+
+    response = {
+        "statusCode": 200,
+        "export": result,
+    }
+
+    return response
